69a0419f7709ebbdea2df750cdbaf626e2081e34
Cinder Biscuits <cinder@cinderblocks.biz>
2019-11-11 20:38:02 -0600

    Convert to .NET Core 2.1 app, still getting things sorted out

--- Corrode/Corrode.cs
+++ Corrode/Corrode.cs
@@ -18,7 +18,6 @@ using System.Globalization;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using System.Management;
 using System.Net;
 using System.Reflection;
 using System.Runtime.InteropServices;
